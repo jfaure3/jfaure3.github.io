a69188914b7c88a24089b09a1734337c86e3ba74
jfaure3 <jfaure3@gmail.com>
2024-04-28 21:44:18 +0200

    chrono et fin
    affiche le chrono et fin réelle des gongs

--- sons.js
+++ sons.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     var audio1 = new Audio("gong.mp3");
     var audio2 = new Audio("gong2.mp3");
+
     var playButton = document.getElementById("playButton");
 
     playButton.addEventListener('click', function() {
         const dropdowns = document.querySelectorAll('.custom-select');
         const selectedValues = [];
+
 
         dropdowns.forEach(dropdown => {
             selectedValues.push(dropdown.value);
@@ -17,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const audio2Duration = 1000 * parseInt(selectedValues[2]);
         const silence2Duration = 1000 * parseInt(selectedValues[3]);
         const totalTime = audio1Duration+audio2Duration+silence1Duration+silence2Duration; // 5 minutes en millisecondes
-        const nbCycles = 5*60*1000/totalTime;
+        const nbCycles = 1.5*60*1000/totalTime;
         // Fonction pour jouer audio1
         function playAudio1() {
             playButton.textContent = "Inspirer";
@@ -41,7 +43,9 @@ document.addEventListener('DOMContentLoaded', function() {
             audio1.pause();
             audio1.currentTime = 0;
             audio2.play();
-            setTimeout((silence2Duration == 0) ? playAudio1 : playSilence2, audio2Duration); // Jouer le silence après audio1
+            if (silence1Duration != 0){
+                setTimeout(playSilence2, audio2Duration); // Jouer le silence après audio1
+            }
         }
 
         // Fonction pour jouer le silence après audio2
@@ -60,9 +64,25 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             else{
                 compteur++;
+                console.log(compteur+"/"+nbCycles);
                 playAudio1(); // Commencer le cycle avec audio1   
             }
         }, totalTime);
+    const depart = new Date();
+    function updateClock() {
+        var now = new Date();
+        const seconde = Math.floor((now.getTime()-depart.getTime())/1000);
+        const minutes = String(Math.floor(seconde/60)).padStart(2, '0');
+        const seconds = String(seconde%60).padStart(2, '0');
+        const timeString = minutes + ':' + seconds;
+    
+        document.getElementById('clock').textContent = timeString;
+    }
+    
+    // Mettre à jour l'horloge toutes les secondes
+    setInterval(updateClock, 1000);
 
     });
+
+    
 });
