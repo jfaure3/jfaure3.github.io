fccd439f1753ceb1222da36d1488605af9f69a05
jfaure3 <jfaure3@gmail.com>
2024-04-28 14:45:39 +0200

    finalisation 1

--- sons.js
+++ sons.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var audio1 = new Audio("gong1.mp3");
+    var audio1 = new Audio("gong.mp3");
     var audio2 = new Audio("gong2.mp3");
     var playButton = document.getElementById("playButton");
 
@@ -16,37 +16,48 @@ document.addEventListener('DOMContentLoaded', function() {
         const silence1Duration = 1000 * parseInt(selectedValues[1]);
         const audio2Duration = 1000 * parseInt(selectedValues[2]);
         const silence2Duration = 1000 * parseInt(selectedValues[3]);
-        const totalTime = 5 * 60 * 1000; // 5 minutes en millisecondes
-
+        const totalTime = audio1Duration+audio2Duration+silence1Duration+silence2Duration; // 5 minutes en millisecondes
+        const nbCycles = 8*1000/totalTime;
         // Fonction pour jouer audio1
         function playAudio1() {
+            playButton.textContent = "Inspirer";
             audio1.play();
-            console.log(1);
             setTimeout(playSilence1, audio1Duration); // Jouer le silence après audio1
         }
 
         // Fonction pour jouer le silence après audio1
         function playSilence1() {
-            console.log(2);
+            playButton.textContent = "bloquer";
+            audio1.pause();
+            audio1.currentTime = 0;
             setTimeout(playAudio2, silence1Duration); // Jouer audio2 après le silence
         }
 
         // Fonction pour jouer audio2
         function playAudio2() {
+            playButton.textContent = "exprirer";
             audio2.play();
-            console.log(3);
             setTimeout(playSilence2, audio2Duration); // Jouer le silence après audio2
         }
 
         // Fonction pour jouer le silence après audio2
         function playSilence2() {
-            console.log(4);
-            setTimeout(playAudio1, silence2Duration); // Revenir à audio1 après le silence
+            playButton.textContent = "bloquer";
+            audio2.pause();
+            audio2.currentTime = 0;
         }
 
-        // Jouer le cycle audio toutes les 3 minutes
-        setInterval(() => {
-            playAudio1(); // Commencer le cycle avec audio1
+        // Jouer le cycle audio toutes les n minutes
+        let compteur = 0;
+        playAudio1();
+        const intervalId = setInterval(() => {
+            if (compteur > nbCycles){
+                clearInterval(intervalId);
+            }
+            else{
+                compteur++;
+                playAudio1(); // Commencer le cycle avec audio1   
+            }
         }, totalTime);
 
 
